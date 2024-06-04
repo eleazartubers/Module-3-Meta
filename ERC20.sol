4840d3cb8b695ec3ff332493af1fe10c8dcd865d
eleazartubers <108503804+eleazartubers@users.noreply.github.com>
2024-06-04 17:15:57 +0800

    ERC20.sol

--- ERC20.sol
+++ ERC20.sol
@@ -1,88 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract MyToken {
-    string public name;
-    string public symbol;
-    uint8 public decimals;
-    uint256 public totalSupply;
-    address public owner;
-    mapping(address => uint256) public balanceOf;
-    mapping(address => mapping(address => uint256)) public allowance;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
-    event Mint(address indexed minter, address indexed to, uint256 value);
-    event Burn(address indexed burner, uint256 value);
+contract ERCToken is ERC20, Ownable {
+    // Event for token minting
+    event TokensMinted(address indexed receiver, uint256 amount);
 
-    constructor(
-        string memory _name,
-        string memory _symbol,
-        uint8 _decimals,
-        uint256 _initialSupply
-    ) {
-        name = _name;
-        symbol = _symbol;
-        decimals = _decimals;
-        totalSupply = _initialSupply * 10**uint256(_decimals);
-        owner = msg.sender;
-        balanceOf[msg.sender] = totalSupply;
+    constructor() ERC20("ERC20", "ETH") Ownable(msg.sender) {
+        // Mint initial supply to the contract deployer
+        _mint(msg.sender, 10000 * 1 ** decimals());
     }
 
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Only the contract owner can call this function");
-        _;
+    // Function to mint new tokens, only callable by the owner
+    function mint(address _to, uint256 _amount) public onlyOwner {
+        _mint(_to, _amount);
+        emit TokensMinted(_to, _amount);
     }
 
-    function transfer(address _to, uint256 _value) external returns (bool success) {
-        require(_to != address(0), "Invalid address");
-        require(balanceOf[msg.sender] >= _value, "Insufficient balance");
-
-        balanceOf[msg.sender] -= _value;
-        balanceOf[_to] += _value;
-
-        emit Transfer(msg.sender, _to, _value);
-        return true;
+    // Function to transfer tokens
+    function transferTokens(address _to, uint256 _amount) public {
+        _transfer(msg.sender, _to, _amount);
     }
 
-    function approve(address _spender, uint256 _value) external returns (bool success) {
-        allowance[msg.sender][_spender] = _value;
-
-        emit Approval(msg.sender, _spender, _value);
-        return true;
-    }
-
-    function transferFrom(address _from, address _to, uint256 _value) external returns (bool success) {
-        require(_from != address(0), "Invalid address");
-        require(_to != address(0), "Invalid address");
-        require(balanceOf[_from] >= _value, "Insufficient balance");
-        require(allowance[_from][msg.sender] >= _value, "Allowance exceeded");
-
-        balanceOf[_from] -= _value;
-        balanceOf[_to] += _value;
-        allowance[_from][msg.sender] -= _value;
-
-        emit Transfer(_from, _to, _value);
-        return true;
-    }
-
-    function mint(address _to, uint256 _value) external onlyOwner {
-        require(_to != address(0), "Invalid address");
-        
-        totalSupply += _value;
-        balanceOf[_to] += _value;
-
-        emit Mint(msg.sender, _to, _value);
-        emit Transfer(address(0), _to, _value);
-    }
-
-    function burn(uint256 _value) external {
-        require(balanceOf[msg.sender] >= _value, "Insufficient balance");
-
-        balanceOf[msg.sender] -= _value;
-        totalSupply -= _value;
-
-        emit Burn(msg.sender, _value);
-        emit Transfer(msg.sender, address(0), _value);
+    // Function to burn tokens
+    function burnTokens(uint256 _amount) public {
+        require(balanceOf(msg.sender) >= _amount, "Insufficient balance");
+        _burn(msg.sender, _amount);
     }
 }
